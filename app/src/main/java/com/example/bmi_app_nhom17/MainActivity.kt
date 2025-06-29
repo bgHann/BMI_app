46561bcddac8756136dd874de3a0ec8ff5e02c22
Han <hanbuii7705@gmail.com>
2025-06-29 13:43:39 +0700

    update

--- app/src/main/java/com/example/bmi_app_nhom17/MainActivity.kt
+++ app/src/main/java/com/example/bmi_app_nhom17/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BMI_app_Nhom17Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BMI_app_Nhom17Theme {
-        Greeting("Android")
-    }
-}
